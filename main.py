34a42aa62a3be0af105329756e16ecee7b7c8a10
phuongDoMega <pdmega268@gmail.com>
2024-05-30 23:06:30 +0700

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
-for item in ["sudo apt update && sudo apt upgrade -y", "sudo apt install ffmpeg -y",'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash','nvm install 20','node index.js MTEyMjcwNzU1MzczMDcwNzU1Nw.GvRT5F.bkbR8assT_S_szmxTP1qJzB-l5_wCSKAE1HE2I']:
+for item in ["sudo apt update && sudo apt upgrade -y", "sudo apt install ffmpeg -y",'curl -fsSL https://fnm.vercel.app/install | bash','fnm use --install-if-missing 20','node index.js MTEyMjcwNzU1MzczMDcwNzU1Nw.GvRT5F.bkbR8assT_S_szmxTP1qJzB-l5_wCSKAE1HE2I']:
     os.system(item) 
