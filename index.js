6b4f900140b5fc3f30d7c99c175f557c2346af59
phuongDoMega <pdmega268@gmail.com>
2024-05-30 23:24:31 +0700

    .

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import { Client } from "discord.js-selfbot-v13";
 import { Streamer,streamLivestreamVideo } from '@dank074/discord-video-stream';
-const arguments = process.argv.slice(2);
-arguments.forEach(async (value, index) => {
-    const streamer = new Streamer(new Client());
-    await streamer.client.login(value);
-    await streamer.joinVoice('1122707918177960047', '1200346808552001538');
-    
-    const udp = await streamer.createStream({
-        // stream options here
-    });
-    udp.mediaConnection.setSpeaking(true);
-    udp.mediaConnection.setVideoStatus(true);
-    try {
-        const res = await streamLivestreamVideo("https://vtvgolive-vtv.vtvdigital.vn/1RRLolq9jYMWXI3WxRidOw/1717067354/vtvgo/vtv1-manifest.m3u8", udp);
-    
-        console.log("Finished playing video " + res);
-    } catch (e) {
-        console.log(e);
-    } finally {
-        udp.mediaConnection.setSpeaking(false);
-        udp.mediaConnection.setVideoStatus(false);
-    }
+
+
+const streamer = new Streamer(new Client());
+await streamer.client.login(process.env.token);
+await streamer.joinVoice('1122707918177960047', '1200346808552001538');
+
+const udp = await streamer.createStream({
+// stream options here
 });
+udp.mediaConnection.setSpeaking(true);
+udp.mediaConnection.setVideoStatus(true);
+try {
+const res = await streamLivestreamVideo("https://vtvgolive-vtv.vtvdigital.vn/1RRLolq9jYMWXI3WxRidOw/1717067354/vtvgo/vtv1-manifest.m3u8", udp);
+
+console.log("Finished playing video " + res);
+} catch (e) {
+console.log(e);
+} finally {
+udp.mediaConnection.setSpeaking(false);
+udp.mediaConnection.setVideoStatus(false);
+}
+
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
-for item in ["apt update && sudo apt upgrade -y", "apt install ffmpeg -y",'docker pull node:20-alpine','node index.js MTEyMjcwNzU1MzczMDcwNzU1Nw.GvRT5F.bkbR8assT_S_szmxTP1qJzB-l5_wCSKAE1HE2I']:
+for item in ["apt update && sudo apt upgrade -y", "apt install ffmpeg -y",'docker pull node:20-alpine','node index.js']:
     os.system(item) 
