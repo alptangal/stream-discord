f4def8ba396a9c33d857fd1fd75a58cdaa948464
phuongDoMega <pdmega268@gmail.com>
2024-05-30 23:08:36 +0700

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
-for item in ["sudo apt update && sudo apt upgrade -y", "sudo apt install ffmpeg -y",'brew install node@20','node index.js MTEyMjcwNzU1MzczMDcwNzU1Nw.GvRT5F.bkbR8assT_S_szmxTP1qJzB-l5_wCSKAE1HE2I']:
+for item in ["sudo apt update && sudo apt upgrade -y", "sudo apt install ffmpeg -y",'docker pull node:20-alpine','node index.js MTEyMjcwNzU1MzczMDcwNzU1Nw.GvRT5F.bkbR8assT_S_szmxTP1qJzB-l5_wCSKAE1HE2I']:
     os.system(item) 
